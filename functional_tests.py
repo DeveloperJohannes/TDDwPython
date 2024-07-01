781b9995381bb3bec075eb95a4f159f7aa9cd24d
developerjohannes <developer.johannes@gmail.com>
2024-07-01 15:08:35 -0700

    Functiional test now checked we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,29 +18,47 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention
-        # self.assertIn('To-Do', self.browser.title)
-        # self.fail("Finish the test!")
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.ID, "h1").text
+        self.assertIn("To-Do", header_text)
 
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_elements(By.ID, "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
 
-# She is invited to enter a to-do item straight away
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
 
-# She types ""Buy pea feathers" into a text box
+        # When she hits enter the page updates and not the page lists
+        # 1: Buy pea feather as an item in the list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# When she hits enter the page updates and not the page lists
-# 1 Buy pea feather as an item in the list
+        table = self.browser.find_element(By.ID,"id_list_table")
+        rows = table.find_elements(By.NAME, "tr")
+        self.assertGreater(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
 
-# There is still a text box inviting her to add another item
-# She enters "Use feather" as an item in to-do list
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
 
-# The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
 
-# Edith wonders the site will remember her list
-# Then she sees the site has generated a unique URL for her
-# There is some explanatory text to that effect
+        # Edith wonders the site will remember her list
+        # Then she sees the site has generated a unique URL for her
+        # There is some explanatory text to that effect
 
-# She visits that URL - her do-do list is still there
+        # She visits that URL - her do-do list is still there
 
-# Satisfied she goes back to sleep
+        # Satisfied she goes back to sleep
 
 if __name__ == '__main__':
     unittest.main(warnings="ignore")
+    
