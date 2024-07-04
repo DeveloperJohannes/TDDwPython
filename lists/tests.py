b1b3dfc4c9a8eb9b0708b819d1526a94d0541e91
developerjohannes <developer.johannes@gmail.com>
2024-07-03 18:07:53 -0700

    interium checkin still getting id_list_table error

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,35 @@
 from django.test import TestCase
 from lists.models import Item
 
-class HomePageTests(TestCase):
 
+class HomePageTest(TestCase):
     def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
     def test_can_save_a_POST_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
-
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = "The first (ever) list item"
